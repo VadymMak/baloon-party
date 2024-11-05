a8657d13dba2c3f22c48114a69f2cecc7a7a1236
VadymMak <makevytssvadym@gmail.com>
2024-11-05 12:58:10 +0100

    Our gallery added_1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 // src/pages/Home.tsx
 import React from "react";
 import Hero from "../components/Hero";
-import Services from "../components/Services";
 import Gallery from "../components/Gallery";
 import Contact from "../components/Contact";
 import About from "../components/About";
