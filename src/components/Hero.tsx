3cd212adcdb652b8cf0fa9ee8d5cd6323119ab4b
VadymMak <makevytssvadym@gmail.com>
2024-11-06 07:09:55 +0100

    font fixed_3

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,8 @@ import styles from "./Hero.module.scss"; // Import the SCSS module
 
 const Hero: React.FC = () => (
   <section className={styles.hero}>
-    <h1>Welcome to Balon Party!</h1>
-    <p>Your go-to place for balloon decorations and party services.</p>
+    <h1>Vitajte na Balónovej Párty!</h1>
+    <p>Vaše miesto pre balónové dekorácie a párty služby.</p>
   </section>
 );
 
