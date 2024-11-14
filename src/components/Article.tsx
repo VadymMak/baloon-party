e8d9abcfac1c2dea9dfb16db41ebc561273e3d90
VadymMak <makevytssvadym@gmail.com>
2024-11-14 13:10:18 +0300

    Added picture about us component_2

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -12,7 +12,7 @@ interface IArticle {
 const Article: React.FC = () => {
   const [articles] = useState<IArticle[]>([
     {
-      title: "Christmas Holiday Celebration",
+      title: "Vianočná sviatočná oslava",
       description:
         "Naša vianočná oslava v Trenčíne sa niesla v duchu teplého a rodinného prostredia. Vianočné balónové dekorácie dokonale zapadli do sviatočnej atmosféry a pomohli vytvoriť nezabudnuteľnú vianočnú oslavu. V Trenčíne a jeho okolí sa tešíme, že môžeme vytvárať krásne spomienky na výnimočné chvíle.",
       imageSrc: "/images/article_1.jpg",
@@ -20,7 +20,7 @@ const Article: React.FC = () => {
       imageLeft: false, // Picture on the right side
     },
     {
-      title: "Halloween Party Celebration",
+      title: "Halloweenska párty oslava",
       description:
         "Vytvorili sme strašidelnú, no zábavnú atmosféru na Halloweenskej párty v Trenčíne, kde sa hostia tešili na tematickú výzdobu a originálne balónové dekorácie. Naša práca zanechala nezabudnuteľný dojem na všetkých účastníkoch. Sme hrdí, že môžeme prinášať čarovnú atmosféru do Trenčína a celého Trenčianskeho kraja.",
       imageSrc: "/images/article_1.jpg",
@@ -32,7 +32,7 @@ const Article: React.FC = () => {
   return (
     <section className={styles.articles}>
       <h1 className={styles.sectionTitle}>
-        Recent Celebrations and Events We've Decorated
+        Nedávne oslavy a podujatia, ktoré sme dekorovali
       </h1>
       {articles.map((article, index) => (
         <div
