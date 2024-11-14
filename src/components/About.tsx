8c57a17c263179616dc8bbb1c423d6510a72090d
VadymMak <makevytssvadym@gmail.com>
2024-11-14 12:04:00 +0300

    Added picture about us component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,24 +3,35 @@ import styles from "./About.module.scss"; // Import SCSS module
 
 const About: React.FC = () => (
   <section className={styles.about}>
-    <h2>O nás</h2>
-    <p>
-      Vitajte na stránke Baloon Party00! Sme profesionáli v tvorbe dekorácií a
-      fotostien pre vaše výnimočné chvíle v Trenčíne a celom Trenčianskom kraji.
-      Naším cieľom je priniesť do vášho sviatočného dňa jedinečnú atmosféru a
-      farebnú krásu, ktorá zanechá spomienky plné radosti a šťastia.
-    </p>
-    <p>
-      Milujeme prinášať svetlé emócie a robiť oslavy nezabudnuteľnými. Či už
-      plánujete svadbu, narodeninovú oslavu, firemný večierok alebo inú
-      špeciálnu udalosť, postaráme sa o to, aby bola výnimočná.
-    </p>
-    <p>
-      Ponúkame tiež možnosť vytvoriť individuálny dizajn presne podľa vašich
-      predstáv. Spolu s vami premeníme vaše nápady na skutočnosť a zabezpečíme,
-      aby výzdoba dokonale ladila s vaším štýlom a charakterom udalosti.
-    </p>
-    <p>Ďakujeme, že nám dôverujete s vašimi najkrajšími momentmi!</p>
+    <div className={styles.textContainer}>
+      <h2>O nás</h2>
+      <p>
+        Vitajte na stránke Baloon Party00! Sme profesionáli v tvorbe dekorácií a
+        fotostien pre vaše výnimočné chvíle v Trenčíne a celom Trenčianskom
+        kraji. Naším cieľom je priniesť do vášho sviatočného dňa jedinečnú
+        atmosféru a farebnú krásu, ktorá zanechá spomienky plné radosti a
+        šťastia.
+      </p>
+      <p>
+        Milujeme prinášať svetlé emócie a robiť oslavy nezabudnuteľnými. Či už
+        plánujete svadbu, narodeninovú oslavu, firemný večierok alebo inú
+        špeciálnu udalosť, postaráme sa o to, aby bola výnimočná.
+      </p>
+      <p>
+        Ponúkame tiež možnosť vytvoriť individuálny dizajn presne podľa vašich
+        predstáv. Spolu s vami premeníme vaše nápady na skutočnosť a
+        zabezpečíme, aby výzdoba dokonale ladila s vaším štýlom a charakterom
+        udalosti.
+      </p>
+      <p>Ďakujeme, že nám dôverujete s vašimi najkrajšími momentmi!</p>
+    </div>
+    <div className={styles.imageContainer}>
+      <img
+        src="/images/my-photo-wall.jpg"
+        alt="Our Decorations"
+        className={styles.image}
+      />
+    </div>
   </section>
 );
 
